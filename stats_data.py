2b74cfeb9bd23f39a5fe352e7d8be63252dd385d
HectorMurBe <hectormb.9@gmail.com>
2016-08-12 21:44:22 -0500

    Update stats_data.py
    some changes, really alpha

--- stats_data.py
+++ stats_data.py
@@ -4,13 +4,64 @@ from __future__ import division, print_function  # Python 2 users only
 import argparse
 import pandas as pd
 import numpy as np
+import tensorflow.contrib.learn as skflow
 
-train_types = {'Agencia_ID':np.uint16, 'Ruta_SAK':np.uint16, 'Cliente_ID':np.uint32, 
-               'Producto_ID':np.uint16, 'Demanda_uni_equil':np.uint32}
+def give_data(path_train,path_test):
+    train_types = {'Semana':np.uint8,'Agencia_ID':np.uint16, 'Ruta_SAK':np.uint16, 'Cliente_ID':np.uint32,'Producto_ID':np.uint16, 'Demanda_uni_equil':np.uint32}
 
-test_types = {'Agencia_ID':np.uint16, 'Ruta_SAK':np.uint16, 'Cliente_ID':np.uint32, 
-              'Producto_ID':np.uint16, 'id':np.uint16}
+    test_types = {'Semana':np.uint8,'Agencia_ID':np.uint16, 'Ruta_SAK':np.uint16, 'Cliente_ID':np.uint32,
+                  'Producto_ID':np.uint16, 'id':np.uint16}
+    df_train = pd.read_csv(path_train, usecols=train_types.keys(), dtype=train_types)
+    df_test = pd.read_csv(path_test,usecols=test_types.keys(), dtype=test_types)
+    return df_train,df_test
+def temp_preproc_weeks(dataframe,size):
+    weeks=[]
+    for i in np.unique(dataframe.Semana):
+        weeks.append(df_train[dataframe.Semana==i].sample(size))
+    return weeks
+def preproc_weeks(dataframe):
+    weeks=[]
 
+    for i in np.unique(dataframe.Semana):
+        weeks.append(df_train[dataframe.Semana==i])
+        #TODO:ORDER DATA by cliente,ruta,producto
+    return weeks
+"""def data_preproces(weeks,logsize):
+    features=[]
+    labels=[]
+    for i in  range(len(weeks)-logsize-1):
+        weeks[i]=pd.concat(weeks[i:(i+logsize)],axis=1)
+    return weeks
+    PROBLEMA AL CONCATENAR
+    """
+def data_preproces(weeks,logsize):
+    #dataframe to matrix
+    for i in range(len(weeks)):
+        weeks[i]=weeks[i].as_matrix()
+    features=[]
+    labels=[]#TODO implement with numpy
+    for i in range(len(weeks)-logsize):
+        weeks[i]=np.concatenate(weeks[i:(i+logsize)], axis=1)
+        #weeks[i]=np.concatenate([weeks[i],weeks[i+logsize][][:-1]],axis=1)
+        for j in range(len(weeks[i])):
+            print (weeks[i][j])
+            print (weeks[i+logsize][j][:-1])
+            feature=np.concatenate([weeks[i][j],weeks[i+logsize][j][:-1]])
+            features.append(feature)
+            labels.append(weeks[i+logsize][j][-1])
+        print (weeks[i])
+    del weeks
+    print (features)
+    print (labels)
+    return np.array(features),np.array(labels)
+
+def model(features,labels,test_size):
+    regressor = skflow.TensorFlowLinearRegressor()#TODO convert uint32 to TensorFlow DType
+    regressor.fit(features[:-test_size], labels[:-test_size])
+    score = metrics.mean_squared_error(regressor.predict(features[-test_size:]), labels[-test_size:])
+    print ("MSE: ")
+    print (score)
+    return regressor
 
 
 
@@ -28,11 +79,8 @@ if __name__ == '__main__':
 
     opts = p.parse_args()
     print("Reading data...")
-    df_train = pd.read_csv(opts.train, usecols=train_types.keys(), dtype=train_types)
-    df_test = pd.read_csv(opts.test,usecols=test_types.keys(), dtype=test_types)
-
-    print("Columns for train")
-    print(df_train.size)
-
-    print("Columns for test")
-    print(df_test.size)
+    df_train,df_test=give_data(opts.train,opts.test)
+    print ("All data readed...")
+    weeks=temp_preproc_weeks(df_train,2)
+    features,labels=data_preproces(weeks,2)
+    model(features,labels,2)
