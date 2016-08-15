da28fdf938cade6c34d14fbcd4c30f74c28c745e
HectorMurBe <hectormb.9@gmail.com>
2016-08-14 20:48:35 -0500

    Update stats_data.py
    Skflow is now working

--- stats_data.py
+++ stats_data.py
@@ -5,26 +5,27 @@ import argparse
 import pandas as pd
 import numpy as np
 import tensorflow.contrib.learn as skflow
+from sklearn import metrics,preprocessing
+
 
 def give_data(path_train,path_test):
     train_types = {'Semana':np.uint8,'Agencia_ID':np.uint16, 'Ruta_SAK':np.uint16, 'Cliente_ID':np.uint32,'Producto_ID':np.uint16, 'Demanda_uni_equil':np.uint32}
 
     test_types = {'Semana':np.uint8,'Agencia_ID':np.uint16, 'Ruta_SAK':np.uint16, 'Cliente_ID':np.uint32,
-                  'Producto_ID':np.uint16, 'id':np.uint16}
+                      'Producto_ID':np.uint16, 'id':np.uint16}
     df_train = pd.read_csv(path_train, usecols=train_types.keys(), dtype=train_types)
     df_test = pd.read_csv(path_test,usecols=test_types.keys(), dtype=test_types)
     return df_train,df_test
 def temp_preproc_weeks(dataframe,size):
     weeks=[]
     for i in np.unique(dataframe.Semana):
-        weeks.append(df_train[dataframe.Semana==i].sample(size))
+        weeks.append(df_train[dataframe.Semana==i].sample(size).apply(lambda x: x.astype("float64")))#convert uint32 to TensorFlow DType
     return weeks
 def preproc_weeks(dataframe):
     weeks=[]
-
     for i in np.unique(dataframe.Semana):
-        weeks.append(df_train[dataframe.Semana==i])
-        #TODO:ORDER DATA by cliente,ruta,producto
+        weeks.append(df_train[dataframe.Semana==i].apply(lambda x: x.astype("float64")))#convert uint32 to TensorFlow DType
+        #TODO:ORDER DATA by cliente,ruta,producto; Weeks have diferent sizes
     return weeks
 """def data_preproces(weeks,logsize):
     features=[]
@@ -44,27 +45,25 @@ def data_preproces(weeks,logsize):
         weeks[i]=np.concatenate(weeks[i:(i+logsize)], axis=1)
         #weeks[i]=np.concatenate([weeks[i],weeks[i+logsize][][:-1]],axis=1)
         for j in range(len(weeks[i])):
-            print (weeks[i][j])
-            print (weeks[i+logsize][j][:-1])
+            #print (weeks[i][j])
+            #print (weeks[i+logsize][j][:-1])
             feature=np.concatenate([weeks[i][j],weeks[i+logsize][j][:-1]])
             features.append(feature)
             labels.append(weeks[i+logsize][j][-1])
         print (weeks[i])
     del weeks
-    print (features)
-    print (labels)
+    #print (features)
+    #print (labels)
     return np.array(features),np.array(labels)
 
 def model(features,labels,test_size):
-    regressor = skflow.TensorFlowLinearRegressor()#TODO convert uint32 to TensorFlow DType
+    features = preprocessing.StandardScaler().fit_transform(features)
+    regressor = skflow.TensorFlowLinearRegressor()
     regressor.fit(features[:-test_size], labels[:-test_size])
     score = metrics.mean_squared_error(regressor.predict(features[-test_size:]), labels[-test_size:])
     print ("MSE: ")
     print (score)
     return regressor
-
-
-
 if __name__ == '__main__':
     p = argparse.ArgumentParser("Statistics data")
     p.add_argument("--train",default="data/train.csv",
@@ -81,6 +80,9 @@ if __name__ == '__main__':
     print("Reading data...")
     df_train,df_test=give_data(opts.train,opts.test)
     print ("All data readed...")
-    weeks=temp_preproc_weeks(df_train,2)
+    weeks=temp_preproc_weeks(df_train,20000)
+    #weeks=preproc_weeks(df_train)
     features,labels=data_preproces(weeks,2)
-    model(features,labels,2)
+    print ("Starting train")
+    model(features,labels,20000)
+    print ("END :D!")
