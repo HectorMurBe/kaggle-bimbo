d7a7c1678d781b4d2858fd9ce3fe86e708fbac0e
HectorMurBe <hectormb.9@gmail.com>
2016-08-26 13:31:28 -0500

    Change log

--- stats_data.py
+++ stats_data.py
@@ -1,123 +1,107 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function  # Python 2 users only
-import numpy as np
+import argparse
 import pandas as pd
+import numpy as np
+#import xgboost as xgb
 import tensorflow.contrib.learn as skflow
 import tensorflow as tf
 from sklearn import metrics,preprocessing
-def temp(data,size):
-    weeks=[]
-    for i in range(10,12):
-        if size:
-            weeks.append(data[data.Semana==i].head(size))
-        else:
-            weeks.append(data[data.Semana==i])
-    data=pd.concat(weeks)
-    data["Demanda_uni_equil"]=0
-    print (data.tail())
-    return data
+from  sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+#import models_tutogithub as tflinear
 
-def verbose(*args,**kargs):
-    print(*args,**kargs)
 
 def rmsle(y, y0):
     assert len(y) == len(y0)
     return np.sqrt(np.mean(np.power(np.log1p(y+1)-np.log1p(y0+1), 2)))
 
-def load_train(filepath):
-    types = {'Semana':np.int8, 'Agencia_ID':np.int16,
+def give_data(path_train,path_test):
+    train_types = {'Semana':np.int8, 'Agencia_ID':np.int16,
              'Ruta_SAK':np.int16, 'Cliente_ID':np.int32, 'Producto_ID':np.int32,
              'Demanda_uni_equil':np.int16}
-    return pd.read_csv(filepath, usecols=types.keys(), dtype=types)
+    test_types = {'Semana':np.int8,'Agencia_ID':np.int16, 'Ruta_SAK':np.int32, 'Cliente_ID':np.int32,
+                      'Producto_ID':np.int16, 'id':np.int16}
+    df_train = pd.read_csv(path_train, usecols=train_types.keys(), dtype=train_types)
+    df_test = pd.read_csv(path_test,usecols=test_types.keys(), dtype=test_types)
+    return df_train,df_test
 
-def load_test(filepath):
-    types = {'id':np.int32,'Semana':np.int8, 'Agencia_ID':np.int16,
-             'Ruta_SAK':np.int16, 'Cliente_ID':np.int32, 'Producto_ID':np.int32 }
-    return pd.read_csv("./data/test.csv", usecols=types.keys(), dtype=types)
+def preproc_weeks(weeks):
+    ordered_weeks=[]
+    for i in reversed(range(5,10)):
+        #Take the weeks you want to log
+        ant_ant=weeks[weeks.Semana==i-2]
+        ant=weeks[weeks.Semana==i-1]
+        act=weeks[weeks.Semana==i]
+        ant=pd.concat([ant,ant_ant])
+        summary={}
 
-def extract_span(weeks,ix_pred,span):
-    verbose("Processing week:",ix_pred)
-    first=True
-    idx_data={}
-    f_=np.zeros([weeks[ix_pred-3].shape[0],span-1+5])
-    l_=np.zeros(weeks[ix_pred-3].shape[0])
-    verbose("Creating matrix for week",f_.shape)
-    semana=ix_pred-3
-    for val,row in enumerate(weeks[semana].itertuples()):
-        idx_data[row[2:6]]=val
-        l_[val]=row[6]
-        f_[val,span-1]=row[7]
-        f_[val,span]=row[8]
-        f_[val,span+1]=row[9]
-        f_[val,span+2]=row[10]
-        f_[val,span+3]=row[11]
-    for i in range(span)[1:span]:
-        semana=ix_pred-i-3
-        verbose("Adding info week",semana+3)
-        for row in weeks[semana].itertuples():
-            try:
-                val=idx_data[row[2:6]]
-                f_[val,span-i-1]=row[6]
-            except:
-                pass
-    return f_,l_
-def extract_span2(weeks,ix_pred,span):
-    verbose("Processing week:",ix_pred)
-    first=True
-    idx_data={}
-    f_=np.zeros([weeks[ix_pred-3].shape[0],span-1+5])
-    verbose("Creating matrix for week",f_.shape)
-    semana=ix_pred-3
-    for val,row in enumerate(weeks[semana].itertuples()):
-        idx_data[row[2:6]]=val
-        f_[val,span-1]=row[7]
-        f_[val,span]=row[8]
-        f_[val,span+1]=row[9]
-        f_[val,span+2]=row[10]
-        f_[val,span+3]=row[11]
-    for i in range(span)[1:span]:
-        semana=ix_pred-i-3
-        verbose("Adding info week",semana+3)
-        for row in weeks[semana].itertuples():
-            try:
-                val=idx_data[row[2:6]]
-                f_[val,span-i-1]=row[6]
-            except:
-                pass
-    return f_
+        summary['lg1_pc']   = ant.groupby(['Producto_ID','Cliente_ID'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        verbose("Calculated lg1_PC")
+        summary['lg1_p']   = ant.groupby('Producto_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg1_P")
+        summary['lg1_c']   = ant.groupby('Cliente_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg1_C")
+        summary['lg1_pa']   = ant.groupby(['Producto_ID','Agencia_ID'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        verbose("Calculated lg1_PA")
+        summary['lg1_pr']   = ant.groupby(['Producto_ID','Ruta_SAK'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        verbose("Calculated lg1_PR")
+        #summary['lg1_a']   = ant.groupby('Agencia_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg1_A")
+        #summary['lg1_r']   = ant.groupby('Ruta_SAK')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg1_R")
+        #summary['lg2_pc']   = ant.groupby(['Producto_ID','Cliente_ID'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg2_PC")
+        #summary['lg2_p']   = ant.groupby('Producto_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg2_P")
+        #summary['lg2_c']   = ant.groupby('Cliente_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg2_C")
+        #summary['lg2_pa']   = ant.groupby(['Producto_ID','Agencia_ID'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg2_PA")
+        #summary['lg2_pr']   = ant.groupby(['Producto_ID','Ruta_SAK'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg2_PR")
+        #summary['lg2_a']   = ant.groupby('Agencia_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg2_A")
+        #summary['lg2_r']   = ant.groupby('Ruta_SAK')['Demanda_uni_equil'].transform(np.mean).astype('float32')
+        #verbose("Calculated lg2_R")
+        #Second merge
+        act['lg1_c']=summary['lg1_c']
+        act['lg1_p']=summary['lg1_p']
+        #act['lg1_a']=summary['lg1_a']
+        #act['lg1_r']=summary['lg1_r']
+        act['lg1_pc']=summary['lg1_pc']
+        act['lg1_pa']=summary['lg1_pa']
+        act['lg1_pr']=summary['lg1_pr']
+        #act['lg2_c']=summary['lg2_c']
+        #act['lg2_p']=summary['lg2_p']
+        #act['lg2_a']=summary['lg2_a']
+        #act['lg2_r']=summary['lg2_r']
+        #act['lg2_pc']=summary['lg2_pc']
+        #act['lg2_pa']=summary['lg2_pa']
+        #act['lg2_pr']=summary['lg2_pr']
+        ordered_weeks.append(act.fillna(0))
+    ordered_weeks.reverse()
+    data=pd.concat(ordered_weeks[:-1])
+    return data,ordered_weeks[-1]
 
-def prepare_train_data(data,test,size=100000):
+def prepare_train_data(data,size=1000000):
     verbose('Isolating weeks')
     weeks=[]
-    for i in range(3,10):
+    for i in range(3,9):
         if size:
-            weeks.append(data[data.Semana==i].head(size))
+            weeks.append(data[data.Semana==i])
         else:
             weeks.append(data[data.Semana==i])
-
+    temp=data[data.Semana==9].head(size)
+    test_labels=temp.Demanda_uni_equil.values
+    temp["Demanda_uni_equil"]=0
+    weeks.append(temp)
     data=pd.concat(weeks)
-    #print (data.tail(50))
+    #test=weeks[-1]
 
 
-    summary={}
-    """
-    data['meanP']   = data.groupby('Producto_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    test['meanP']   = data.groupby('Producto_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    verbose("Calculated meanP")
-    data['meanC']   = data.groupby('Cliente_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    test['meanC']   = data.groupby('Cliente_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    verbose("Calculated meanC")
-    data['meanPA']  = data.groupby(['Producto_ID','Agencia_ID'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    test['meanPA']  = data.groupby(['Producto_ID','Agencia_ID'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    verbose("Calculated meanPA")
-    data['meanPR']  = data.groupby(['Producto_ID','Ruta_SAK'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    test['meanPR']  = data.groupby(['Producto_ID','Ruta_SAK'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
-    verbose("Calculated meanPR")
-    data['meanPC'] = data.groupby(['Producto_ID','Cliente_ID'])['Demanda_uni_equil'].transform('mean')
-    test['meanPC'] = data.groupby(['Producto_ID','Cliente_ID'])['Demanda_uni_equil'].transform('mean')
-    verbose("Calculated meanPC")
-    """
+
     data['meanP']   = data.groupby('Producto_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
     verbose("Calculated meanP")
     data['meanC']   = data.groupby('Cliente_ID')['Demanda_uni_equil'].transform(np.mean).astype('float32')
@@ -126,106 +110,72 @@ def prepare_train_data(data,test,size=100000):
     verbose("Calculated meanPA")
     data['meanPR']  = data.groupby(['Producto_ID','Ruta_SAK'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
     verbose("Calculated meanPR")
-    data['meanPC'] = data.groupby(['Producto_ID','Cliente_ID'])['Demanda_uni_equil'].transform('mean')
+    data['meanPC'] = data.groupby(['Producto_ID','Cliente_ID'])['Demanda_uni_equil'].transform(np.mean).astype('float32')
     verbose("Calculated meanPC")
-    test_ = data.loc[:, ['Producto_ID', 'meanP']].drop_duplicates(subset=['Producto_ID'])
-    test = test.merge(test_,how='left',on=['Producto_ID'],copy=False)
-    test_ = data.loc[:, ['Cliente_ID', 'meanC']].drop_duplicates(subset=['Cliente_ID'])
-    test = test.merge(test_,how='left',on=['Cliente_ID'],copy=False)
-    test_ = data.loc[:, ['Producto_ID','Agencia_ID','meanPA']].drop_duplicates(subset=['Producto_ID','Agencia_ID'])
-    test = test.merge(test_,how='left',on=['Producto_ID','Agencia_ID'],copy=False)
-    test_ = data.loc[:, ['Producto_ID','Ruta_SAK','meanPR']].drop_duplicates(subset=['Producto_ID','Ruta_SAK'])
-    test = test.merge(test_,how='left',on=['Producto_ID','Ruta_SAK'],copy=False)
-    test_ = data.loc[:, ['Producto_ID','Cliente_ID','meanPC']].drop_duplicates(subset=['Producto_ID','Cliente_ID'])
-    test = test.merge(test_,how='left',on=['Producto_ID','Cliente_ID'],copy=False)
-    del test_
-
-    verbose("Calculated meanPC")
-    test.fillna(0, inplace=True)
-    data=pd.concat([data,test])
-    #print (data.tail())
-
-
     verbose(data.info(memory_usage=True))
 
-    verbose('Isolating weeks (again)')
-    ids=data[data.tst==1]["id"].values
-    data.drop(["tst","id"],inplace=True,axis=1)
-    print ("Data before order")
-    data=data[["Semana",'Agencia_ID','Cliente_ID','Producto_ID','Ruta_SAK','Demanda_uni_equil','meanP','meanC','meanPA',"meanPR",'meanPC']]
-    print (data.head(50))
-    weeks=[]
-    for i in range(3,12):
-        weeks.append(data[data.Semana==i])
-    #test_size=weeks[-3].shape[0]
-    data=pd.concat(weeks)
-    print (data.tail(50))
-    del data
-    print (">>>>>>>>>>>>>>>IDS")
-    print (ids)
-    print (">>>>>>>>>>>>>>>Order")
-    print (weeks[0].head(50))
 
-    return weeks,ids
-def predweeks(weeks,regressor,span=3):
-    features=extract_span2(weeks[:-1],5,span)
-    predictions10=predict(features,regressor)
-    print (predictions10)
-    weeks[-2]["Demanda_uni_equil"]=predictions10
-    features=extract_span2(weeks[1:],5,span)
-    predictions20=predict(features,regressor)
-    print (predictions20)
-    return np.concatenate((predictions10, predictions20))
+    data,test=preproc_weeks(data)
+    train_labels=data.Demanda_uni_equil.values
 
-def writesubmision(ids,predictions,path="./data/submission.csv"):
-    submision=pd.DataFrame({"Demanda_uni_equil":predictions,"id":ids.astype("int32")})
-    submision.to_csv(path,index=False)
+    data.drop(["Producto_ID","Ruta_SAK","Cliente_ID","Agencia_ID","Semana","Demanda_uni_equil"],inplace=True,axis=1)#Not usefull columns from here
+    test.drop(["Producto_ID","Ruta_SAK","Cliente_ID","Agencia_ID","Semana","Demanda_uni_equil"],inplace=True,axis=1)#Not usefull columns from here
+    return data.as_matrix(),train_labels,test.as_matrix(),test_labels
 
-def getraindata(weeks,span=3):
-    verbose('Extracting span')
-    feats=[]
-    labels=[]
-    for i in range(3+span-1,10):
-        feats_,labels_=extract_span(weeks,i,span)
-        feats.append(feats_)
-        labels.append(labels_)
+""" POSSIBLE SOLUTION FOR TEST
+    test_=data.loc[:,['Producto_ID',"meanP"]]
+    test=pd.merge(test,test_,how="left",on=["Producto_ID"])
+    test_=data.loc[:,['Cliente_ID',"meanPC"]]
+    test=pd.merge(test,test_,how="left",on=["Cliente_ID"])
+    test_=data.loc[:,['Producto_ID','Agencia_ID',"meanPA"]]
+    test=pd.merge(test,test_,how="left",on=['Producto_ID','Agencia_ID'])
+    test_=data.loc[:,['Producto_ID','Ruta_SAK',"meanPR"]]
+    test=pd.merge(test,test_,how="left",on=['Producto_ID','Ruta_SAK'])
+    test_=data.loc[:,['Producto_ID','Cliente_ID',"meanPC"]]
+    test=pd.merge(test,test_,how="left",on=['Producto_ID','Cliente_ID'])
 
-    return np.vstack(feats),np.hstack(labels)
-def train(features,labels):
-    regressor = skflow.TensorFlowLinearRegressor()#TODO convert uint32 to TensorFlow DType
-    verbose ("Training...")
-    regressor.fit(features, labels)
-    return regressor
-def predict(features,regressor):
-    preds=regressor.predict(features)
-    preds[preds<0]=0
-    return preds
+    #remove excess columns... in final version won't ve neaded"""
 
-def train_test(features,labels,test_size):
+
+def verbose(*args,**kargs):
+    print(*args,**kargs)
+
+
+def train_test(features,labels,features_test,labels_test):
     verbose ("Features size",features.shape)
     verbose ("Labels size",labels.shape)
-    verbose ("Size test",test_size)
-    regressor = skflow.TensorFlowLinearRegressor()#TODO convert uint32 to TensorFlow DType
+    verbose ("Features size",features_test.shape)
+    verbose ("Labels size",labels_test.shape)
+    #T_train_xgb = xgb.DMatrix(features, labels)
     verbose ("Training...")
-    regressor.fit(features[:-test_size], labels[:-test_size])
+    #params = {"objective":"reg:linear",
+    #                               "booster" : "gbtree",
+    #                               "eta":0.1,
+    #                               "max_depth":10,
+    #                               "subsample":0.85,
+    #                               "colsample_bytree":0.7}
+    #gbm = xgb.train(dtrain=T_train_xgb,params=params)
+    regressor = skflow.TensorFlowLinearRegressor()#TODO convert uint32 to TensorFlow DType
+    regressor.fit(features, labels)
     verbose ("Predict...")
-    preds=regressor.predict(features[-test_size:])
+    #preds=gbm.predict(xgb.DMatrix(features_test))
+    preds=regressor.predict(features_test)
     preds[preds<0]=0
     verbose(preds)
     verbose(len(preds))
     verbose("MSE: ")
-    score = metrics.mean_squared_error(preds, labels[-test_size:])
+    score = metrics.mean_squared_error(preds,labels_test)
     verbose("Original",score)
-    score = metrics.mean_squared_error(np.round(preds), labels[-test_size:])
+    score = metrics.mean_squared_error(np.round(preds), labels_test)
     verbose("round",score)
     verbose ("RMSLE:")
-    score = rmsle(preds, labels[-test_size:])
+    score = rmsle(preds, labels_test)
     verbose ("Original",score)
-    score = rmsle(np.round(preds), labels[-test_size:])
+    score = rmsle(np.round(preds), labels_test)
     verbose ("round",score)
 
+
 if __name__ == '__main__':
-    import argparse
     p = argparse.ArgumentParser("Statistics data")
     p.add_argument("--train",default="data/train.csv",
             action="store", dest="train",
@@ -233,40 +183,13 @@ if __name__ == '__main__':
     p.add_argument("--test",default="data/test.csv",
             action="store", dest="test",
             help="Train file [test.csv]")
-    p.add_argument("--test_size",default=1000,type=int,
-            action="store", dest="test_size",
-            help="Size of records per week [all]")
-    p.add_argument("--size",default=4000000,type=int,
-            action="store", dest="size",
-            help="Size of records per week [all]")
-    p.add_argument("--span",default=3,type=int,
-            action="store", dest="span",
-            help="Span of weeks")
     p.add_argument("-v", "--verbose",
             action="store_true", dest="verbose",
             help="Verbose mode [Off]")
 
     opts = p.parse_args()
-
-    # Prepara función de verbose  -----------------------------------------
-    if not opts.verbose:
-       verbose = lambda *a: None
-
-    verbose("Loading training data...")
-    df_train=load_train(opts.train)
-    df_train["tst"]=0
-    verbose("Loading test data...")
-    df_test=load_test(opts.train)
-    df_test["tst"]=1
-    df_test=temp(df_test,False)
-    print (df_test.head(20))
-    weeks,ids=prepare_train_data(df_train,df_test,size=opts.size)
-    features,labels=getraindata(weeks[:-2],span=3)
-    """print ("Test size >>>>>>>>>>>>")
-    print (test_size)
-    train_test(features,labels,test_size)"""
-    regressor=train(features,labels)
-    predictions=predweeks(weeks[-4:],regressor)
-    print ("Pedicciones juntas: ")
-    print (predictions)
-    writesubmision(ids,predictions)
+    print("Reading data... ")
+    df_train,df_test=give_data(opts.train,opts.test)
+    print ("All data readed... ")
+    features,labels,features_test,labels_test=prepare_train_data(df_train)
+    train_test(features,labels,features_test,labels_test)
